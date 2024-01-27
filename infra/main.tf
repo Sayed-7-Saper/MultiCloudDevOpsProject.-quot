7518475d2e1b026cc2e3ba7ce8a3fd7d7a56d831
Elsayed Saber <elkot616@gmail.com>
2024-01-27 16:31:53 +0200

    upload code

---
+++ infra/main.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+    source = "./vpc"
+   # vpc_id = module.vpc.vpc_id
+}
+
+module "ec2" {
+    source = "./ec2"
+  # vpc_id = module.vpc.vpc_id
+  # public_subnet_id = module.vpc.public_subnet_id
+}
+  
